7a9745c22f001a09ab2ce3a933af38551ad29715
tientulac <tientulac@users.noreply.github.com>
2020-05-12 21:29:19 +0700

    lap 4-

--- lam bt tren lop.cpp
+++ lam bt tren lop.cpp
@@ -2,16 +2,17 @@
 #include <math.h>
 
 int main() {
-	int a,i;
-	printf("nhap vao so can tinh: ");
-	scanf("%d",&a);
-	int n1=a,n2=0;
-	for (i=0;i<4;i++){
-		n2=n2*10+n1%10;
-		n1=n1/10;
-	}
-	printf("nghich dao cua so nay la: %d",n2);
+	int n;
+	printf("nhap n=\n");
+	scanf("%d",&n);
 	
+	int n2=0;
 	
+	// n!= 0
+	for(;n!=0;n/=10){
+	// nhanh dung
+	n2=n2*10+n%10;
 }
-
+	// nhanh sai - display n2
+	printf("ngich dao: %d",n2);
+ }
